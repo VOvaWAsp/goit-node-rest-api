5db3bff6cd8e25d8420735213d196bf2954c6196
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2024-03-31 21:05:29 -0500

    test

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,97 @@
-import contactsService from "../services/contactsServices.js";
+import path from "path"
+import {promises as fs} from "fs"
+import { nanoid } from "nanoid";
 
-export const getAllContacts = (req, res) => {};
+import { createContactSchema, updateContactSchema } from "../schemas/ contactsSchemas.js";
+import { getContactById, listContacts, removeContact } from "../services/ contactsServices.js";
+import validateBody from "../helpers/validateBody.js";
+import HttpError from "../helpers/HttpError.js";
+// import contactsService from "../services/contactsServices.js";
 
-export const getOneContact = (req, res) => {};
+const contactsPath = path.join("db", "contacts.json");
 
-export const deleteContact = (req, res) => {};
+export const getAllContacts = async(req, res) => {
+    const getUsers = await listContacts();
+    res.status(200).json({
+        msg: 'success',
+        users: getUsers,
+    })
+};
 
-export const createContact = (req, res) => {};
+export const getOneContact = async(req, res) => {
+    const { id } = req.params;
+    const getUser = await getContactById(id);
+    
+    HttpError(404, {message: "Not found"})
 
-export const updateContact = (req, res) => {};
+    res.status(200).json({
+        msg: 'success',
+        users: getUser,
+    })};
+
+export const deleteContact = async(req, res) => {
+    const { id } = req.params;
+    const removeContacted = await removeContact(id);
+
+    HttpError(404, {message: "Not found"});
+
+    res.status(200).json({
+        msg: "success",
+        user: removeContacted
+    })
+};
+
+export const createContact = async(req, res) => {
+const { value, errors } = createContactSchema.validate(req.body);
+
+if (errors) {
+    validateBody(errors);
+};
+
+const { name, email, phone }= value;
+
+try {
+    const addJsonById = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(addJsonById);
+    const newContact = { id: nanoid(), name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+
+    res.status(201).json({
+        msg: "success",
+        user: newContact,
+    });
+} catch (err) {
+        console.error('Error creating contact:', err);
+        res.status(500).json({ error: 'Server error' });
+    }
+};
+
+export const updateContact = async(req, res) => {
+    const { id } = req.params;
+    const { value, errors } = updateContactSchema.validate(req.body);
+
+    if (errors) {
+        HttpError(400, {"message": "Body must have at least one field"});
+    }
+
+    const { name, email, phone } = value;
+
+    try {
+    const updateJsonById = await fs.readFile(contactsPath);
+    const get = JSON.parse(updateJsonById)
+    const find = get.filter((item) => item.id === id)
+    if (!find) {
+       return null
+    }
+    const update = { ...get[find], id, name, email, phone}
+    await fs.writeFile(contactsPath, JSON.stringify(get))
+    res.status(200).json({
+        msg: "success",
+        users: update,
+    })
+} catch (err) {
+    console.error('Error creating contact:', err);
+    res.status(500).json({ error: 'Server error' });
+}
+};
--- services/ contactsServices.js
+++ services/ contactsServices.js
@@ -1,5 +1,41 @@
-node_modules/
-.env
-.idea
-.vscode
-.DS_Store
+import {promises as fs} from "fs"
+import path from "path"
+
+const contactsPath = path.join("db", "contacts.json");
+
+export async function listContacts() {
+        const getJson = await fs.readFile(contactsPath);
+    return JSON.parse(getJson);
+  }
+  
+  export async function getContactById(contactId) {
+    const getJsonById = await fs.readFile(contactsPath)
+    const get = JSON.parse(getJsonById)
+    const find = get.find((item) => item.id === contactId)
+    if (!find) {
+       return null
+    }
+    return find;
+}
+  
+  export async function removeContact(contactId) {
+    const removeJsonById = await fs.readFile(contactsPath);
+    const get = JSON.parse(removeJsonById)
+    const find = get.findIndex((item) => item.id === contactId)
+    if (find === -1) {
+       return null
+    }
+    const removed = get.splice(find, 1)[0];
+    await fs.writeFile(contactsPath, JSON.stringify(get))
+    return removed;
+  }
+  
+//   export async function addContact(name, email, phone) {
+//         const addJsonById = await fs.readFile(contactsPath);
+//         const get = JSON.parse(addJsonById)
+//         const addJson = {id: nanoid(), name, email, phone};
+//         const updateJson = [...get, addJson];
+//         await fs.writeFile(contactsPath, JSON.stringify(updateJson))
+//         return addJson
+//   }
+
